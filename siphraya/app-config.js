fa704095e7818dbb1d1ccb9ace6e097216f09fbc
edudole <mintanaeducation@gmail.com>
2026-09-25 11:39:23 +0700

    Add files via upload

---
+++ siphraya/app-config.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+
+  window.APP_CONFIG = Object.freeze({
+    API_URL: 'https://script.google.com/macros/s/AKfycby4zrYBs2oTyzy_NdWxRimVFTEJUdvb6_TclonS4MlFLlVqmOdaYFucnVnXV3FaFPQs_A/exec'
+  });
+})();
