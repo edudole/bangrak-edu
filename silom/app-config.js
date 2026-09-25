7e19d3038fa023860b1992688f1dc5aec45f9be1
edudole <mintanaeducation@gmail.com>
2026-09-25 11:35:25 +0700

    Add files via upload

---
+++ silom/app-config.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+
+  window.APP_CONFIG = Object.freeze({
+    API_URL: 'https://script.google.com/macros/s/AKfycbzdxC1rPWTuehzRtKKgzKOhiwFLYz5i6-e4Ak9wsDnRd3lMtNnI2KqKS90PdOPUPsVH/exec'
+  });
+})();
