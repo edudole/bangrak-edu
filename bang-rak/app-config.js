a6145f9b4c3511ead32ae50c771766f7f4ef754f
edudole <mintanaeducation@gmail.com>
2026-09-25 11:28:08 +0700

    Add files via upload

---
+++ bang-rak/app-config.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+
+  window.APP_CONFIG = Object.freeze({
+    API_URL: 'https://script.google.com/macros/s/AKfycbwuKYgVZRYIbujm1hHv4lQchRIPaKN-X300y7jOHgcNyvK_60-CFOzadomro0e4HVS59A/exec'
+  });
+})();
